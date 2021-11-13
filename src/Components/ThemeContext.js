ea86da84072e5d86347c2d8fe5dc91c497746c81
gamerdudesnip3r <jw29731@gmail.com>
2021-11-13 10:51:59 -0500

    Save light/dark mode setting for user

--- src/Components/ThemeContext.js
+++ src/Components/ThemeContext.js
@@ -1,13 +1,15 @@
 import React, { createContext, useState } from 'react'
 
 const ThemeContext = createContext();
+const localStorageTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light';
 
 function ThemeContextProvider(props) {
 
-    const [ theme, setTheme ] = useState('light');
+    const [ theme, setTheme ] = useState(localStorageTheme);
 
     const toggleTheme = () => {
         setTheme(theme === 'light' ? 'dark' : 'light');
+        localStorage.setItem('theme', theme === 'light' ? 'dark' : 'light');
     }
 
     return (
