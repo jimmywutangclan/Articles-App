3bc1068b5c7030b9f65b308d062104f50fd524d4
gamerdudesnip3r <jw29731@gmail.com>
2021-11-12 20:53:08 -0500

    Added functionality to open up an article

--- src/Components/ArticleApp.js
+++ src/Components/ArticleApp.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Routes } from 'react-router';
 import ArticlePreview from './ArticlePreview'
 import { ThemeContextConsumer, ThemeContextProvider } from './ThemeContext'
 import ToggleButton from './ToggleButton'
 
 export default function ArticleApp(props) {
-    const articleHtml = props.articles.map((article) => <ArticlePreview title={article.title} body={article.body} />);
+    let x = 0;
+    const articleHtml = props.articles.map((article) => <ArticlePreview title={article.title} body={article.body} id={x++}/>);
 
     return (
         <ThemeContextConsumer>
